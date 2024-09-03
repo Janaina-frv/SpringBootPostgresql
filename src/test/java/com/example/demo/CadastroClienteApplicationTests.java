f8e621872d0d3e2a1f3e60ad967085ec355af6e3
Janaina de Fatima Ramos Vasconcelos <janaina.vasconcelos1@ibm.com>
2024-09-03 15:20:42 -0300

    Testando classe principal

--- src/test/java/com/example/demo/CadastroClienteApplicationTests.java
+++ src/test/java/com/example/demo/CadastroClienteApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CadastroClienteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() throws SQLException {
+		CadastroClienteApplication.main(new String[] {});
 	}
 
 }
